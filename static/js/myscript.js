d565c17f1a70f9923ca4d1f44db4dc007b0e068b
giordifungula <jodi.fungula@gmail.com>
2019-08-18 22:07:34 +0300

    added the partials folder and html files in there

---
+++ static/js/myscript.js
@@ -0,0 +1,4 @@
+let side_image = document.getElementById("side-image");
+alert("Hey")
+side_image.src = "https://avatars2.githubusercontent.com/u/27930281?s=400&u=2dfe2d181c37b7c5829c14f198ec329389724d1b&v=4"
+// update the 
