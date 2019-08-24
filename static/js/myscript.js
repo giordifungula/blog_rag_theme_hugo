f8cb5fc18cf5d23fed0326a8e5b75f80e2459418
giordifungula <jodi.fungula@gmail.com>
2019-08-24 16:54:49 +0300

    added the distortion code

--- static/js/myscript.js
+++ static/js/myscript.js
@@ -1,10 +1,10 @@
-// let side_image = document.getElementById("side-image").src;
-alert("Hey")
-alert("loaded")
+
 var myAnimation = new hoverEffect({
     parent: document.querySelector('.my-div'),
     intensity: 0.3,
     image1: 'https://images.pexels.com/photos/424670/pexels-photo-424670.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
     image2: 'https://images.pexels.com/photos/757889/pexels-photo-757889.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-    displacementImage: 'https://images.pexels.com/photos/250591/pexels-photo-250591.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+    displacementImage: 'https://raw.githubusercontent.com/robin-dela/hover-effect/master/images/stripe1.png'
 });
+
+console.log(myAnimation)
