a9976eb0a32338a4e2de502b408ed63d315b5233
giordifungula <jodi.fungula@gmail.com>
2019-08-24 15:33:16 +0300

    updated image on landing page

--- static/js/myscript.js
+++ static/js/myscript.js
@@ -1,4 +1,10 @@
-// let side_image = document.getElementById("side-image");
-// // alert("Hey")
-// side_image.src = "https://avatars2.githubusercontent.com/u/27930281?s=400&u=2dfe2d181c37b7c5829c14f198ec329389724d1b&v=4"
-// // update the 
+// let side_image = document.getElementById("side-image").src;
+alert("Hey")
+alert("loaded")
+var myAnimation = new hoverEffect({
+    parent: document.querySelector('.my-div'),
+    intensity: 0.3,
+    image1: 'https://images.pexels.com/photos/424670/pexels-photo-424670.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    image2: 'https://images.pexels.com/photos/757889/pexels-photo-757889.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    displacementImage: 'https://images.pexels.com/photos/250591/pexels-photo-250591.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+});
